d33197dc8bd45bb3acc62a215cff4c56d12296f9
David Orobon <david.orobon@madisonmk.com>
2025-05-22 19:24:24 +0200

    Laravel y /check

---
+++ routes/api.php
@@ -0,0 +1,9 @@
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/check', function () {
+    return response()->json([
+        'status' => 'ok',
+        'message' => 'La API funciona correctamente.'
+    ]);
+});
